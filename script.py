909412a7ab9e5d0f1cd43c0aa73da0ad3525b014
Vojtěch Kletečka <vojta.kletecka@gmail.com>
2018-03-16 18:42:23 +0100

    Fixed issue/unintended-behavior
    It is possible that people might think that Pepa is clever and
    recognize his IQ in range 110-125. As his IQ is around 135 inteligent
    sounds much more appropriate.

--- script.py
+++ script.py
@@ -2,4 +2,7 @@
 
 # Pepa píše program
 
-# Pepa je chytrý, proto vyvíjí v dev
+# Pepa je inteligentní, proto vyvíjí v dev
+
+# Pepa si mezitím, co Franta vyvíjí novou featuru, všiml, že program je špatně napsaný a potenciálně se v něm nachází případ, který by mohl vést na nezamýšlené chování.
+# Přepsal část kódu tak, aby k tomuto případu nemohlo dojít.
