959f4f76f6385b30e2e5f25b735128555ca6b8dc
Vojtěch Kletečka <vojta.kletecka@gmail.com>
2018-03-16 20:07:23 +0100

    Specifications
    Added specifications for important function.

---
+++ importantFunction.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+# Amálka je zkušená a chce předejít merge konfliktům, proto založila nový soubor a v něm bude programovat novou funkci.
+# Protože ví, že nebylo specifikováno, kde se má funkce programovat, předtím, než začne dá potřebné info Frantovi, který použije její funkci ve svém kódu
